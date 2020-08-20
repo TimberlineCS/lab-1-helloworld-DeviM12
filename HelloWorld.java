eb999e2553b27b605d4c8c0a8e476b19e8da3e0a
DeviM12 <devi.medicharla2022@gmail.com>
2020-08-20 17:43:27 +0000

    done!

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld{
+//Devi Alekhya Medicharla
+//AP Computer Science
+//8/20/2020
+
+  public static void main(String args[]){
+    System.out.println("                              v   v  v   ");
+    System.out.println("                              v   v  v     ");
+    System.out.println("                         v    v   v  v   v");
+    System.out.println("                           v  v v v v v v");
+    System.out.println("                             vvvvvvvvvv");
+    System.out.println(" 0               0           vvvvvvvvvvv");
+    System.out.println("                              vvvvvvvvv");
+    System.out.println("         L.                    vvvvvvv");
+    System.out.println("                     ____________vv");
+    System.out.println("               1        ");
+    System.out.println("              1");
+    System.out.println("      _______1");
+  }
+}
